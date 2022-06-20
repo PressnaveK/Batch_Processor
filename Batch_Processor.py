aba2e109f3408bef36ddf655826922ba45c62c88
PressnaveK <pressnave2@gmail.com>
2022-06-20 10:55:42 +0530

    Initial

---
+++ Batch_Processor.py
@@ -0,0 +1,87 @@
+import os
+import cv2
+import shutil
+
+logic = True
+while logic:
+    try:
+        #Get file name
+        fileName = str(input("\n Video (.mp4/.mov)>>> ")).strip()
+        cap= cv2.VideoCapture(fileName)
+        #Get fps and total frames
+        fps = int(cap.get(cv2.CAP_PROP_FPS))
+        #Get number sample to make directories
+        totalFrame =int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        logic = False
+    except:
+        print("<ERROR> The file does not excist or invalid file format")
+
+#Get number sample to make directories
+logic = True
+while logic:
+    try:
+        numSample = int(input("\nNum of Sample >>> "))
+        if numSample > 0:logic = False
+        else:print("<ERROR> Not be negative or zero")
+    except:
+        print("<ERROR> Invalid input")
+
+
+for i in range(1,numSample+1):
+    dirName = f"Level-{i}"
+    if os.path.exists(dirName):
+        shutil.rmtree("./"+dirName,ignore_errors=True)
+        os.mkdir(dirName)
+    else:
+        os.mkdir(dirName)
+#Get startFrame and endFrame
+logic = True
+while logic:
+    try:
+        start,end = str(input("\nStart and end(sec) _,_>>> ")).strip().split(',')
+        if (int(start) >= 0) or (int(end) >= 0) :logic = False
+        else:print("<ERROR> Invalid input")
+    except:
+      print("<ERROR> Invalid input")  
+
+logic = True
+while logic:
+    try:
+        width,height = str(input("\nWidth and height(px) _,_>>> ")).strip().split(',')
+        if (int(width) >= 0) or (int(height) >= 0) :logic = False
+        else:print("<ERROR> Invalid input")
+    except:
+        print("<ERROR> Invalid input")  
+print("\n Process getting started...")
+
+startFrame = int(start)*fps
+endFrame = startFrame if startFrame != 0 else  0
+
+endFrame = int(end)*fps
+endFrame = endFrame if endFrame != 0 else  totalFrame
+
+val = (endFrame - startFrame) / numSample
+frameCount = 0
+while frameCount <= endFrame:
+    ret,frame = cap.read()
+    if frameCount >= startFrame:
+        err = 0.5 - (1/val)
+        count = frameCount - startFrame 
+        print(f"\r Extracting.....{count-1} / {endFrame - startFrame -1} images",end = "\r")
+        dircount = round((count /val) + err)
+        output = f"Level-{dircount}/Img-{count-1}.jpg"
+        resized_img = cv2.resize(frame,(int(width),int(height)))
+        cv2.imwrite(output,resized_img)
+
+    else: print(f"\r Triming.... {frameCount} / {startFrame}",end = "\r")
+    frameCount += 1
+print(f"\n Finished Batch processing.....\n | {count-1} images extracted into {numSample} samples |\n")
+
+
+
+
+
+
+
+
+
---
+++ test.py
@@ -0,0 +1,3 @@
+num1,num2 = str(input("Enter:")).strip().split(',')
+print(num1,num2)
+print(type(num1))
